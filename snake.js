637dbf84d8500a374790f8a052e22fb6714f7615
rozi <rozbeh24@outlook.com>
2021-03-14 20:42:11 +0330

    tail

--- snake.js
+++ snake.js
@@ -2,7 +2,12 @@ var canvas = document.getElementById("myCanvas");
 const ctx = canvas.getContext('2d');
 let snakebody = [];
 let gol = [0, 0]
-snakebody.push([0, 0])
+snakebody.push(
+    { x: 0, y: 50 },
+    { x: 50, y: 50 },
+    { x: 100, y: 50 },
+    { x: 150, y: 50 },
+)
 let swap = false
 for (let indexy = 0; indexy < 800; indexy += 50) {
     for (let index = 0; index <= 1500; index += 50) {
@@ -11,75 +16,77 @@ for (let indexy = 0; indexy < 800; indexy += 50) {
         swap = !swap
     }
 }
-ctx.beginPath();
-ctx.arc(snakebody[snakebody.length - 1][0] + 25, snakebody[snakebody.length - 1][1] + 25, 20, 0, 2 * Math.PI);
-ctx.fillStyle = 'red'
-ctx.fill();
-ctx.lineWidth = 5;
-ctx.strokeStyle = '#003300';
-ctx.stroke();
-
-let Move;
-document.body.addEventListener("keydown", event => {
-
-    switch (event.code) {
-        case 'KeyD':
-            clearInterval(Move)
-           // drawsnake(50, 0)
-            Move = setInterval(() => {
-                drawsnake(50, 0)
-            }, 500);
-            break;
-        case 'KeyA':
-            clearInterval(Move)
-            Move = setInterval(() => {
-                drawsnake(-50, 0)
-            }, 500);
-            break;
-            case 'KeyW':
-                clearInterval(Move)
-               // drawsnake(50, 0)
-                Move = setInterval(() => {
-                    drawsnake(0, -50)
-                    
-                }, 500);
-                break;
-            case 'KeyS':
-                clearInterval(Move)
-                Move = setInterval(() => {
-                    drawsnake(0, 50)
-                }, 500);
-                break;
-
-        default:
-            break;
-    }
 
 
-});
-
-function drawsnake(x, y) {
-    snakebody[snakebody.length - 1][0] += x
-    snakebody[snakebody.length - 1][1] += y
+for (let index = 0; index < snakebody.length; index++) {
     ctx.beginPath();
-    ctx.arc(snakebody[snakebody.length - 1][0] + 25, snakebody[snakebody.length - 1][1] + 25, 20, 0, 2 * Math.PI);
+    ctx.arc(snakebody[index].x + 25, snakebody[index].y + 25, 20, 0, 2 * Math.PI);
     ctx.fillStyle = 'red'
     ctx.fill();
     ctx.lineWidth = 5;
     ctx.strokeStyle = '#003300';
     ctx.stroke();
-    ctx.beginPath();
-    var p = ctx.getImageData(5 + snakebody[snakebody.length - 1][0] - x, snakebody[snakebody.length - 1][1] - y, 1, 1).data;
-    const HexColor = "#" + ((1 << 24) + (p[0] << 16) + (p[1] << 8) + p[2]).toString(16).slice(1);
-    ctx.arc(25 + snakebody[snakebody.length - 1][0] - x, 25 + snakebody[snakebody.length - 1][1] - y, 20, 0, 2 * Math.PI);
-    ctx.fillStyle = HexColor
-    ctx.fill();
-    ctx.lineWidth = 10;
-    ctx.strokeStyle = HexColor
-    ctx.stroke();
-    for (let index = 0; index < snakebody.length - 1; index++) {
-        snakebody[index] = snakebody[index + 1]
-    }
+}
 
+
+let Move;
+document.body.addEventListener("keydown", event => {
+const interval=100
+    switch (event.code) {
+        case 'KeyD':
+            clearInterval(Move)
+            Move = setInterval(() => {
+                drawsnake(50, 0)
+            }, interval);
+            break;
+        case 'KeyA':
+            clearInterval(Move)
+            Move = setInterval(() => {
+                drawsnake(-50, 0)
+            }, interval);
+            break;
+        case 'KeyW':
+            clearInterval(Move)
+            Move = setInterval(() => {
+                drawsnake(0, -50)
+            }, interval);
+            break;
+        case 'KeyS':
+            clearInterval(Move)
+            Move = setInterval(() => {
+                drawsnake(0, 50)
+            }, interval);
+            break;
+        default:
+            break;
+    }
+});
+
+function drawsnake(x, y) {
+    for (let index = 0; index < snakebody.length - 1; index++) {
+        ctx.beginPath();
+        var p = ctx.getImageData(5 + snakebody[index].x , 5+ snakebody[index].y , 1, 1).data;
+        const HexColor = "#" + ((1 << 24) + (p[0] << 16) + (p[1] << 8) + p[2]).toString(16).slice(1);
+        ctx.arc(25 + snakebody[index].x, 25 + snakebody[index].y , 20, 0, 2 * Math.PI);
+        ctx.fillStyle = HexColor
+        ctx.fill();
+        ctx.lineWidth = 10;
+        ctx.strokeStyle = HexColor
+        ctx.stroke();
+        snakebody[index].x = parseInt(snakebody[index + 1].x)
+        snakebody[index].y = parseInt(snakebody[index + 1].y)
+    }
+    snakebody[snakebody.length - 1].x += x
+    snakebody[snakebody.length - 1].y += y
+
+        for (let index = 0; index < snakebody.length; index++) {
+            ctx.beginPath();
+            ctx.arc(snakebody[index].x + 25, snakebody[index].y + 25, 20, 0, 2 * Math.PI);
+            ctx.fillStyle = 'red'
+            ctx.fill();
+            ctx.lineWidth = 5;
+            ctx.strokeStyle = '#003300';
+            ctx.stroke();
+        }
 
 }
