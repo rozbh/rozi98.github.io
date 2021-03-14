acd13a82f38f29d9cb98e65f06210469a81595ee
rozi <rozbeh24@outlook.com>
2021-03-14 21:37:40 +0330

    90%

--- snake.js
+++ snake.js
@@ -1,7 +1,9 @@
 var canvas = document.getElementById("myCanvas");
+
+
 const ctx = canvas.getContext('2d');
 let snakebody = [];
-let gol = [0, 0]
+
 snakebody.push(
     { x: 0, y: 50 },
     { x: 50, y: 50 },
@@ -16,7 +18,17 @@ for (let indexy = 0; indexy < 800; indexy += 50) {
         swap = !swap
     }
 }
-
+let gol = {
+    x: (Math.floor(Math.random() * 29) + 1) * 50,
+    y: (Math.floor(Math.random() * 15) + 1) * 50
+}
+ctx.beginPath();
+ctx.arc(gol.x + 25, gol.y + 25, 20, 0, 2 * Math.PI);
+ctx.fillStyle = 'pink'
+ctx.fill();
+ctx.lineWidth = 5;
+ctx.strokeStyle = '#003300';
+ctx.stroke();
 
 for (let index = 0; index < snakebody.length; index++) {
     ctx.beginPath();
@@ -28,10 +40,9 @@ for (let index = 0; index < snakebody.length; index++) {
     ctx.stroke();
 }
 
-
 let Move;
 document.body.addEventListener("keydown", event => {
-const interval=100
+    const interval = 100
     switch (event.code) {
         case 'KeyD':
             clearInterval(Move)
@@ -61,13 +72,33 @@ const interval=100
             break;
     }
 });
-
+let score = 0
+console.log(snakebody[snakebody.length - 1]);
+console.log('gol is', gol);
 function drawsnake(x, y) {
+
+    if (JSON.stringify(snakebody[snakebody.length - 1]) == JSON.stringify(gol)) {
+        snakebody.push(gol)
+        var p = document.getElementById("score")
+        gol = {
+            x: (Math.floor(Math.random() * 28) + 1) * 50,
+            y: (Math.floor(Math.random() * 14) + 1) * 50
+        }
+        ctx.beginPath();
+        ctx.arc(gol.x + 25, gol.y + 25, 20, 0, 2 * Math.PI);
+        ctx.fillStyle = 'pink'
+        ctx.fill();
+        ctx.lineWidth = 5;
+        ctx.strokeStyle = '#003300';
+        ctx.stroke();
+        score += 1
+        p.innerText = `your score is dddd:${score}`
+    }
     for (let index = 0; index < snakebody.length - 1; index++) {
         ctx.beginPath();
-        var p = ctx.getImageData(5 + snakebody[index].x , 5+ snakebody[index].y , 1, 1).data;
+        var p = ctx.getImageData(5 + snakebody[index].x, 5 + snakebody[index].y, 1, 1).data;
         const HexColor = "#" + ((1 << 24) + (p[0] << 16) + (p[1] << 8) + p[2]).toString(16).slice(1);
-        ctx.arc(25 + snakebody[index].x, 25 + snakebody[index].y , 20, 0, 2 * Math.PI);
+        ctx.arc(25 + snakebody[index].x, 25 + snakebody[index].y, 20, 0, 2 * Math.PI);
         ctx.fillStyle = HexColor
         ctx.fill();
         ctx.lineWidth = 10;
@@ -78,15 +109,14 @@ function drawsnake(x, y) {
     }
     snakebody[snakebody.length - 1].x += x
     snakebody[snakebody.length - 1].y += y
-
-        for (let index = 0; index < snakebody.length; index++) {
-            ctx.beginPath();
-            ctx.arc(snakebody[index].x + 25, snakebody[index].y + 25, 20, 0, 2 * Math.PI);
-            ctx.fillStyle = 'red'
-            ctx.fill();
-            ctx.lineWidth = 5;
-            ctx.strokeStyle = '#003300';
-            ctx.stroke();
-        }
+    for (let index = 0; index < snakebody.length; index++) {
+        ctx.beginPath();
+        ctx.arc(snakebody[index].x + 25, snakebody[index].y + 25, 20, 0, 2 * Math.PI);
+        ctx.fillStyle = 'red'
+        ctx.fill();
+        ctx.lineWidth = 5;
+        ctx.strokeStyle = '#003300';
+        ctx.stroke();
+    }
 
 }
